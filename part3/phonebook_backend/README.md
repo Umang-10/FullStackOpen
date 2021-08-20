e6e97e0306720aca8a2c257aea7f11214ec8a8de
Umang <umangkaushik46.bs@gmail.com>
2021-08-20 16:07:33 +0530

    part3

---
+++ part3/phonebook_backend/README.md
@@ -0,0 +1,5 @@
+My Heroku Applications:
+
+[Backend](https://enigmatic-mesa-74364.herokuapp.com/api/persons)
+
+[FullStack](https://enigmatic-mesa-74364.herokuapp.com)
---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,139 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
+
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        JSON.stringify(req.body)
+    ].join(' ')
+}))
+
+const requestLogger = (request, response, next) => {
+    console.log('Method: ', request.method)
+    console.log('Path:   ', request.path)
+    console.log('Body:   ', request.body)
+    console.log('-----')
+    next()
+}
+
+app.use(requestLogger)
+
+app.get('/api/persons', (request, response) => {
+    Person.find({}).then(people => {
+        response.json(people)
+    })
+})
+
+app.get('/info', (request, response) => {
+    Person.find({}).then(people => {
+        response.send(
+            `
+            <p>Phonebook has info for ${people.length} people</p>
+            <p>${Date()}</p>
+            `
+        )
+    })
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (body.name === undefined) {
+        return response.status(400).json({
+            error: 'Name Missing'
+        })
+    }
+
+    if (body.number === undefined) {
+        return response.status(400).json({
+            error: 'Number Missing'
+        })
+    }
+
+    // if (people.map(p => p.name).includes(body.name)) {
+    //     return response.status(400).json({
+    //         error: 'Name must be unique'
+    //     })
+    // }
+
+    const person = new Person ({
+        name: body.name,
+        number: body.number,
+    })
+
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send( { error: 'unknown endpoint' } )
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send( { error: 'malformatted error' } )
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
